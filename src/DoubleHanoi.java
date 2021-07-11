46bad849ed22434965d5846ee8f58162a3568ee1
Al-Rou <roal.24.alro@gmail.com>
2021-07-10 20:25:31 -0400

    Completed

--- src/DoubleHanoi.java
+++ src/DoubleHanoi.java
@@ -5,7 +5,7 @@ public class DoubleHanoi {
     public static void main(String[] args)
     {
         Solution sol = new Solution();
-        int[] A = {1,5,5};
-        System.out.println(sol.solution(A, 2, 4));
+        int[] A = {1,2,3,6,7,8,11};
+        System.out.println(sol.solution(A, 9, 10));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,76 +1,89 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Solution {
     public int solution(int[] A, int L, int R)
     {
-        int[] auxiliary = new int[A.length+2];
+        List<Integer> auxiliary = new ArrayList<>();
         for(int i = 0; i < A.length; i++)
         {
-            auxiliary[i] = A[i];
+            auxiliary.add(A[i]);
         }
-        auxiliary[A.length] = L;
-        auxiliary[A.length+1] = R;
-        for(int i = 0; i < auxiliary.length-1; i++)
+        int[] subArrayLeft = new int[A.length];
+        int leftCounter = 0;
+        for(int i = 0; i < auxiliary.size(); i++)
         {
-            for(int j = i+1; j < auxiliary.length; j++)
-            {
-                if(auxiliary[i] > auxiliary[j])
-                {
-                    int temp = auxiliary[i];
-                    auxiliary[i] = auxiliary[j];
-                    auxiliary[j] = temp;
+                if (auxiliary.get(i) < L) {
+                    subArrayLeft[leftCounter] = auxiliary.get(i);
+                    leftCounter++;
+                    auxiliary.remove(i);
+                    i--;
+                    if(auxiliary.size() == 0)
+                    {
+                        break;
+                    }
                 }
-            }
-        }
-        int indexOfL = 0;
-        for(int i = 0; i < auxiliary.length; i++)
-        {
-            if(auxiliary[i] == L)
-            {
-                indexOfL = i;
-                break;
-            }
-        }
-        int indexOfR = 0;
-        for(int i = 0; i < auxiliary.length; i++)
-        {
-            if(auxiliary[i] == R)
-            {
-                indexOfR = i;
-                break;
-            }
-        }
-        int[] subArrayLeft = new int[indexOfL];
-        for(int i = 0; i < indexOfL; i++)
-        {
-            subArrayLeft[i] = auxiliary[i];
         }
         int repeatedCounterLeft = 0;
         for(int i = 0; i < subArrayLeft.length-1; i++)
         {
-            if(subArrayLeft[i] == subArrayLeft[i+1])
+            if((subArrayLeft[i] == 0) && (subArrayLeft[i] != subArrayLeft[i+1]))
             {
                 repeatedCounterLeft++;
             }
-        }
-        int leftAnswer = subArrayLeft.length - repeatedCounterLeft;
-
-        int[] subArrayRight = new int[indexOfR];
-        for(int i = indexOfR+1; i < auxiliary.length; i++)
-        {
-            subArrayRight[i-indexOfR-1] = auxiliary[i];
-        }
-        int repeatedCounterRight = 0;
-        for(int i = 0; i < subArrayRight.length-1; i++)
-        {
-            if(subArrayRight[i] == subArrayRight[i+1])
+            else if(subArrayLeft[i] == subArrayLeft[i+1])
             {
-                repeatedCounterRight++;
+                auxiliary.add(subArrayLeft[i]);
+                repeatedCounterLeft++;
             }
         }
-        int rightAnswer = subArrayRight.length - repeatedCounterRight;
+        if(subArrayLeft[subArrayLeft.length-1] == 0)
+        {
+            repeatedCounterLeft++;
+        }
+        int leftAnswer = subArrayLeft.length - repeatedCounterLeft;
+        int rightAnswer = 0;
+        int[] subArrayRight = new int[A.length];
+        int rightCounter = 0;
+        if(auxiliary.size() != 0) {
+            for (int i = 0; i < auxiliary.size(); i++) {
+                if (auxiliary.get(i) > R) {
+                    subArrayRight[rightCounter] = auxiliary.get(i);
+                    rightCounter++;
+                    auxiliary.remove(i);
+                    i--;
+                    if(auxiliary.size() == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+            int repeatedCounterRight = 0;
+            for (int i = 0; i < subArrayRight.length - 1; i++) {
+                if ((subArrayRight[i] == subArrayRight[i + 1]) || (subArrayRight[i] == 0)) {
+                    repeatedCounterRight++;
+                }
+            }
+            if (subArrayRight[subArrayRight.length - 1] == 0) {
+                repeatedCounterRight++;
+            }
+            rightAnswer = subArrayRight.length - repeatedCounterRight;
+        }
 
-        return (leftAnswer+rightAnswer);
+        for(int i = 0; i < subArrayLeft.length; i++)
+        {
+            System.out.println(subArrayLeft[i]);
+        }
+        System.out.println("--------");
+        for(int i = 0; i < subArrayRight.length; i++)
+        {
+            System.out.println(subArrayRight[i]);
+        }
+        System.out.println("--------");
+
+        return (leftAnswer + rightAnswer);
+
+
     }
 }
